d9fe06764a61b0eb36610eaf5d3ab6404a51e5b3
Calum Bradley <calumbradley@Calums-MBP.home>
2022-12-30 22:08:31 +0000

    working mui dashboard spa

---
+++ src/pages/EmployeesHome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EmployeesHome = () => {
+  return (
+    <div>Employees</div>
+  )
+}
+
+export default EmployeesHome
---
+++ src/pages/PerformanceHome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PerformanceHome = () => {
+  return (
+    <div>Performance</div>
+  )
+}
+
+export default PerformanceHome
